94d4fec813202c16c5873f10c7c6871f2c7130ec
Kishan28Kumar <86763404+Kishan28Kumar@users.noreply.github.com>
2023-03-17 20:08:22 +0530

    Add files via upload

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import Nav from "../Nav";
+import Footer from '../Footer';
+
+function About(){
+    return (
+        <>
+            <Nav />
+            <div>
+                
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default About;
